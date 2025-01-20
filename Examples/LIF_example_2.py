b3bd8e054786a16b8d3581275e848ff9e59376e3
Erik <erikjackmuelller@gmail.com>
2025-01-20 17:12:16 +0100

    [UPD]: prepared LIF_example_2.py for implementation of the time varying rate in poisson process

--- Examples/LIF_example_2.py
+++ Examples/LIF_example_2.py
@@ -7,22 +7,28 @@ from Model.LIF import LIF_population
 matplotlib.use('TkAgg')
 
 def input_sine_function(t):
-    v0 = 0.7e2 #.7e2 # .7
+    v0 = 700
     f = 10
     return v0 * (1 + np.sin(2*np.pi*f*t/1000))
+
+def input_step_function(t):
+    res = np.zeros_like(t)
+    amp = 1.2e2
+    res[(t>10) & (t<80)] = amp
+    return res
 
 T = 100
 dt = 0.1
 t = np.arange(0.0, T, dt)
 in_sine = input_sine_function(t)
 
-w0 = 30
-dim = 1000
+w0 = 1
+dim = 100
 con = w0*(np.ones((dim, dim)) - np.eye(dim))
 # con = w0*np.random.uniform(size=(dim, dim))
 # np.fill_diagonal(con, 0)
-lif = LIF_population(T=T, tau_m=20, tref=3,  weights=con, n_neurons=dim, Iext=in_sine, verbose=0)
-lif.gen_poisson_spikes_input(rate=1.0, i_max=1e4, delay=True) #input in nA? #rate of 0.5 seems to be fitting for nykamp plot
+lif = LIF_population(T=T, tau_m=20, tref=3,  weights=con, n_neurons=dim, Iext=input_step_function(t), verbose=0)
+lif.gen_poisson_spikes_input(rate=1.0, i_max=1e2, delay=True) #input in nA? #rate of 0.5 seems to be fitting for nykamp plot
 lif.run()
 
 # Visualize
@@ -34,7 +40,7 @@ lif.raster_plot()
 # lif.plot_voltage_hist(times=times)
 # neuron_num = [0, 2, 5, 12, 22]
 # lif.plot_firing_rate(bin_size=20, smoothing=True)
-lif.plot_populations(bins=1000, smoothing=True, sigma=15)
+lif.plot_populations(bins=1000, smoothing=True, sigma=5, hide_refractory=True)
 
 print(f'neuron 1 spikes: {lif.rec_spikes[0].shape}')
 print(f'neuron 2 spikes: {lif.rec_spikes[1].shape}')
