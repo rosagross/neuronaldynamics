a007f054d1d7057fc2731310c67381b756e714bd
erik <erik.mueller@tu-ilmenau.de>
2025-10-28 20:57:30 +0100

    [ADD]: added more functions for GA optimizer

--- Examples/Rosenbrock_test.py
+++ Examples/Rosenbrock_test.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
 
 from Optimizers.Testfunctions import RosenbrockND
 from Optimizers.Optimizer import Hierarchical_Random, GA
@@ -64,15 +66,48 @@ opt_params['simulate'] = testfunction.simulate
 opt_params['bounds'] = [[-2, 1], [-1, 3]]
 opt_params['x_out'] = 'y'
 opt_params['reference'] = 0
-opt_params['n_iter'] = 5
-opt_params['N1'] = 200
+opt_params['n_iter'] = 20
+opt_params['N1'] = 20
+opt_params['N1'] = 20
 opt_params['tolerance'] = 1e18
 optimizer = GA(parameters=opt_params)
 # opt_params['max_iter'] = 1000
 # optimizer = Hierarchical_Random(parameters=opt_params)
 optimizer.run()
 optimal_param = optimizer.optimum
+# optimizer.plot_fit()
+ps = optimizer.parameter_evolution
 opt_Rosenbrock = Rosenbrock_2D({'x1': optimal_param[0], 'x2': optimal_param[1]})
 opt_Rosenbrock.simulate()
 y_opt = opt_Rosenbrock.y
 print(f'y_opt = {y_opt}')
+
+def animate_sol(params):
+    X, Y = np.mgrid[-2:2.1:0.01, -1:3.1:0.01]
+    xs = np.array((X, Y))
+    y = RosenbrockND(xs)
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    z_min, z_max = y.min(), y.max()
+    c = ax.pcolormesh(X, Y, y, cmap='jet', vmin=z_min, vmax=z_max)
+    sc = ax.scatter(0, 0, marker='x', s=20, c='white')
+    ax.scatter(1, 1, marker='x', s=20, c='red')
+    y_min = np.argmin(y)
+    y_flat = y.flatten()
+    y_min_val = y_flat[y_min]
+    print(f'Min value: {y_min_val}')
+    # a = np.where(y==0)
+    fig.colorbar(c, ax=ax)
+
+    def update(frame):
+        xs = params[frame]
+        sc.set_offsets(xs)
+        return (sc,)
+
+    ani = animation.FuncAnimation(fig=fig, func=update, frames=params.shape[0], interval=1)
+    # plt.show()
+    ani.save(filename='rosenbrock.gif', writer="pillow")
+    plt.close()
+
+
+animate_sol(np.array(ps))
