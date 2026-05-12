8391b9a25387d4a6b39e9ed6bad818acfda769d9
erik <erik.mueller@tu-ilmenau.de>
2026-05-12 07:33:16 +0200

    [UPD]: updated Load_E  to show some EMG data

--- EMG_data/Load_EMG_data_example.py
+++ EMG_data/Load_EMG_data_example.py
@@ -9,7 +9,9 @@ import h5py
 import os
 from scipy.ndimage import gaussian_filter1d
 import matplotlib.pyplot as plt
-hdf5_path = nextcloud_path = 'C:\\Users\\User\\nextcloud\\TMS Neuro Projects\\M1_modeling\\DI_wave_data\\DIwaves_Di_Lazzaro'
+# hdf5_path = 'C:\\Users\\User\\nextcloud\\TMS Neuro Projects\\M1_modeling\\DI_wave_data\\DIwaves_Di_Lazzaro'
+hdf5_path = '/home/erik/Nextcloud_Uni/TMS Neuro Projects/M1_modeling/DI_wave_data/extracted_DI_waves/DiLazarro_di_wave_data.hdf5'
+
 
 
 measurement_dict = dict(orientation='PA', threshold=140, year=2020, hdf5_path=hdf5_path, sigma=1.0)
@@ -19,8 +21,22 @@ if not os.path.exists(hdf5_path):
     raise ValueError('hdf5_path does not exist')
 
 with h5py.File(hdf5_path, 'r') as h5file:
-    name_h5group = h5file[measurement_dict['orientation']][measurement_dict['threshold_type']][str(measurement_dict['threshold'])][
+    name_h5group = h5file[measurement_dict['orientation']]['RMT'][str(measurement_dict['threshold'])][
         str(measurement_dict['year'])]
-    emg_data_full = np.array(name_h5group['EMG']['signal_full'])
-
-a=1
+    name_keys = list(name_h5group.keys())
+    # pick first name here (index 0) since in the dataset there is only one recording per orientation/RMT/year combination
+    emg_data_full = np.array(name_h5group[name_keys[0]]['EMG']['signal_full'])
+    emg_data_mean = np.array(name_h5group[name_keys[0]]['EMG']['signal_mean'])
+    time = np.array(name_h5group[name_keys[0]]['time'])
+emg_data_full = emg_data_full[0].T # formatting from the original .mat files into a useful shape
+plt.plot(time, emg_data_mean, label='mean')
+for i in range(emg_data_full.shape[0]):
+    if i ==0:
+        # add name for legend on first occurence
+        plt.plot(time,emg_data_full[i], c='k', alpha=0.3, zorder=-1, label='trials')
+    else:
+        plt.plot(time, emg_data_full[i], c='k', alpha=0.3, zorder=-1)
+plt.xlabel('time ( ms)')
+plt.ylabel('EMG (mV)')
+plt.legend()
+plt.show()
